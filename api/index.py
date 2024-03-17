c44c9dfcb55c35c73b65df23f7f5c6a1bec217ce
Self-Taught-Fiodor <fiodorchoupletsov@yahoo.fr>
2024-03-17 15:49:46 +0100

    "big try two"

--- api/index.py
+++ api/index.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from forms import SignUpForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "7V89?-_uPQkSLKss"
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",content="Home page")
+
+@app.route("/signup")
+def signup():
+    form = SignUpForm()
+    return render_template("signup.html", form=form)
 
 @app.route("/login")
 def login():
-    return "TODO : LOGIN PAGE"
+    return render_template("login.html")
 
 @app.route("/inputs")
 def inputs():
-    return "TODO : INPUTS PAGE"
+    return render_template("inputs.html")
 
 @app.route("/resistance")
 def resistance():
-    return "TODO : RESISTANCE TRAINING PROGRAMME PAGE"
+    return render_template("resistance.html")
 
 @app.route("/info")
 def info():
-    return "TODO : PHASES AND LINKS describing them PAGE"
+    return render_template("info.html")
 
 @app.route("/outputs")
 def outputs():
-    return "TODO : OUTPUTS PAGE select time periode and get back tracked info"
+    return render_template("outputs")
 
 if __name__ == "__main__":
     app.run(debug=True)
